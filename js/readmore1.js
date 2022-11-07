6b8d6f87f4669ee0efd6bba997bdabd76aea9384
Pratik  Phirke <pratikphirke@MacBook-Air.local>
2022-11-07 01:37:06 -0600

    ...

---
+++ js/readmore1.js
@@ -0,0 +1,15 @@
+function myFunction1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
